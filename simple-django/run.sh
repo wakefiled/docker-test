9466de7650ba797efb5843379f61ac40db2fac14
jskim8 <jskim8@humaxdigital.com>
2019-08-01 10:12:40 +0900

    [wakefiled] Modified django framework command scripts.
     - Simplify Makefile commands.
     - Add run command list: start, stop, connect

--- simple-django/run.sh
+++ simple-django/run.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-docker run --rm -it -v $PWD/test:/work -p 8080:8080 hello:django
+NAME=hello
+TAG=django
+IMAGE_NAME=$NAME:$TAG
+CONTAINER_NAME=my-$NAME-$TAG
+INNER_PORT=8080
+OUTER_PORT=8080
+
+command=$1
+
+case $command in
+	connect)
+		docker run --rm -it -v $PWD/test:/work -p $INNER_PORT:$OUTER_PORT --name $CONTAINER_NAME $IMAGE_NAME bash
+		;;
+	start)
+		docker run --rm -it -v $PWD/test:/work -p $INNER_PORT:$OUTER_PORT --name $CONTAINER_NAME $IMAGE_NAME
+		;;
+	stop)
+		docker container rm -f $CONTAINER_NAME
+		;;
+esac
